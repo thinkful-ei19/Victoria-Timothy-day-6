7de7698b2a4b4014c1ff7d1c9bbfba351683c9cc
Victoria Daug <victoria.daug@gmail.com>
2018-02-26 21:26:56 +0000

    namespacing part3

--- scripts/store.js
+++ scripts/store.js
@@ -2,8 +2,45 @@
 
 // eslint-disable-next-line no-unused-vars
 const store = (function () {
-   
-   
+
+  const findById = function(id) {
+    console.log('findById run')
+    const foundItem = store.items.find(item => items.id === id);
+    console.log(foundItem, 'foundItem from store');
+    return foundItem
+  }
+
+  const addItems = function(name) {
+    try {
+        Item.validateName(name)
+        Item.create(name)
+        store.items.push(name)
+    }
+      catch(error) {
+        console.log('Cannot add item: name not valid');
+    }
+
+  // const findAndToggleChecked = function(id) {
+  //   this.findById(id);
+  //   foundItem.checked = !foundItem.checked;
+  // }
+  //
+  // const findAndUpdateName = function(id, newName) {
+  //   try {
+  //       Item.validateName(newName)
+  //       Item.findById(id)
+  //       this.name = newName
+  //   }
+  //     catch(error) {
+  //       console.log('Cannot update name: {error.message}');
+  //   }
+  // }
+  //
+  // const findAndDelete = function(id){
+  //   const index = store.items.findIndex(item => items.id === id)
+  //   store.items.splice(index, 1);
+  // }
+
     const items = [
         { id: cuid(), name: 'apples', checked: false },
         { id: cuid(), name: 'oranges', checked: false },
@@ -14,9 +51,10 @@ const store = (function () {
     const searchTerm = '';
 
       return {
-         items : items, 
+         items : items,
          hideCheckedItems : hideCheckedItems,
-         searchTerm : searchTerm
+         searchTerm : searchTerm,
+         findById : findById,
+         addItems : addItems
       };
 }() );
-
