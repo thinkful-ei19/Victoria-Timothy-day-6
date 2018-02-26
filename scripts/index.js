1815356a4b31850d2673c5926cc0868c8b362a8a
Timothy Song <tsong0220@gmail.com>
2018-02-26 13:27:49 -0700

    moified addItemToShoppingList function

--- scripts/index.js
+++ scripts/index.js
@@ -4,15 +4,7 @@
 
 
 $(document).ready(function() {
-  const itemNames = [ '', 'pasta', 'sugar' ];
-  itemNames.forEach(name => {
-    try {
-      Item.validateName(name);
-      store.items.push(Item.create(name));
-    } catch(error) {
-      console.log('Cannot add item: ' + error.message);
-    }
-  });
+
   shoppingList.render();
   shoppingList.bindEventListeners();
   shoppingList.render();
