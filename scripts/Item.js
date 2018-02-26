2ac50925987a7ff9a9bfe10d1fe27fe08a6cc1a1
Victoria Daug <victoria.daug@gmail.com>
2018-02-26 19:22:27 +0000

    edited js files

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,9 +1,20 @@
 const Item = (function(){
+  function validateName(name) {
+    if (name === undefiend ){
+      throw new TypeError (console.log("Name does not exist."))
+    }
+  }
 
-   
+ function create(name) {
+   return itemObj = {
+     id : cuid(),
+     name : '',
+     checked : false
+   }
+ }
+    return {
+      validateName()
+      create()
+    };
 
-    return {};
-  
   }());
-
- 
--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,16 @@
 
 
 $(document).ready(function() {
+  const itemNames = [ '', 'apples', 'pears' ];
+  itemNames.forEach(name => {
+    try {
+      Item.validateName(name);
+      store.items.push(Item.create(name));
+    } catch(error) {
+      console.log('Cannot add item: ' + error.message);
+    }
+  });
+  shoppingList.render();
   shoppingList.bindEventListeners();
   shoppingList.render();
 });
-
-
